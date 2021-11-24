669040ee2ded9fd7a3c03f0036755c334a4a8d5c
도재경 <jaydo1204@gmail.com>
2021-11-24 23:43:07 +0900

    feat: Register input  validation 추가

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { mobile } from "../styles/responsive";
 import { register } from "../redux/apiCalls";
@@ -6,7 +6,12 @@ import { useHistory } from "react-router";
 
 const messages = {
   success: "Register was successful. Please continue with login.",
-  password: "Confirm password does not match.",
+  noUsername: "Fill in username",
+  noEmail: "Fill in email address",
+  noPassword: "Fill in password",
+  noConfirmPassword: "Fill in confirm password",
+  passwordNoMatch: "Confirm password does not match.",
+  wrongEmail: "Email format not valid.",
   duplicateEmail: "Email already exists.",
   duplicateUsername: "Username already exists.",
 };
@@ -16,10 +21,85 @@ const Register = () => {
     username: "",
     email: "",
     password: "",
+    confirmPassword: "",
   });
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [readyForSubmit, setReadyForSubmit] = useState(false);
   const history = useHistory();
+
+  // input focusout 발생 시점에 빈 input 여부 확인
+  // 이 방식은 registerInfo가 변경될 때 마다 event handler를 죽였다 다시 적용함으로 퍼포먼스에 좋지 않을것 같음
+  useEffect(() => {
+    const focusOutFunc = (e) => {
+      if (e.target.tagName !== "INPUT") return;
+      const type = e.target.name;
+
+      if (registerInfo[type] === "") {
+        const propName = "no" + type.charAt(0).toUpperCase() + type.slice(1);
+        setErrorMessage((prev) => {
+          return { ...prev, [type]: messages[propName] };
+        });
+      } else {
+        if (type === "email") {
+          const emailValid = /\S+@\S+\.\S+/.test(registerInfo[type]);
+          if (!emailValid) {
+            setErrorMessage((prev) => {
+              return { ...prev, [type]: messages.wrongEmail };
+            });
+          } else {
+            setErrorMessage((prev) => {
+              return { ...prev, [type]: "" };
+            });
+          }
+        } else if (type === "confirmPassword" || type === "password") {
+          if (
+            registerInfo.password !== "" &&
+            registerInfo.password !== registerInfo.confirmPassword
+          ) {
+            setErrorMessage((prev) => {
+              return {
+                ...prev,
+                confirmPassword: messages.passwordNoMatch,
+                password: "",
+              };
+            });
+          } else {
+            setErrorMessage((prev) => {
+              return { ...prev, confirmPassword: "", password: "" };
+            });
+          }
+        } else {
+          setErrorMessage((prev) => {
+            return { ...prev, [type]: "" };
+          });
+        }
+      }
+    };
+    document.addEventListener("focusout", focusOutFunc);
+    return () => {
+      document.removeEventListener("focusout", focusOutFunc);
+    };
+  }, [registerInfo]);
+
+  useEffect(() => {
+    // Check if submit ready
+    let isReady = true;
+    for (let key in errorMessage) {
+      if (errorMessage[key] !== "") isReady = false;
+    }
+
+    if (isReady) {
+      for (let key in registerInfo) {
+        if (registerInfo[key] === "") isReady = false;
+      }
+    }
+    setReadyForSubmit(isReady);
+  }, [registerInfo, errorMessage]);
 
   const handleChange = (e) => {
     setRegisterInfo((prev) => {
@@ -27,31 +107,27 @@ const Register = () => {
     });
   };
 
-  const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (registerInfo.password === confirmPassword) {
-      try {
-        await register(registerInfo);
-        alert(messages.success);
-        history.push("/login");
-      } catch (err) {
-        switch (err) {
-          case "email":
-            setErrorMessage(messages.duplicateEmail);
-            break;
-          case "username":
-            setErrorMessage(messages.duplicateUsername);
-            break;
-          default:
-            console.log(err);
-        }
+    if (errorMessage !== "") return;
+    const newUserInfo = { ...registerInfo };
+    delete newUserInfo.confirmPassword;
+    console.log(newUserInfo);
+    try {
+      await register(newUserInfo);
+      alert(messages.success);
+      history.push("/login");
+    } catch (err) {
+      switch (err) {
+        case "email":
+          setErrorMessage(messages.duplicateEmail);
+          break;
+        case "username":
+          setErrorMessage(messages.duplicateUsername);
+          break;
+        default:
+          console.log(err);
       }
-    } else {
-      setErrorMessage(messages.password);
     }
   };
 
@@ -66,32 +142,37 @@ const Register = () => {
             name="username"
             placeholder="username"
           />
+          {errorMessage.username && <Error>{errorMessage.username}</Error>}
           <Input
             onChange={handleChange}
             value={registerInfo.email}
             name="email"
             placeholder="email"
           />
+          {errorMessage.email && <Error>{errorMessage.email}</Error>}
           <Input
             onChange={handleChange}
-            type="password"
             value={registerInfo.password}
+            type="password"
             name="password"
             placeholder="password"
           />
+          {errorMessage.password && <Error>{errorMessage.password}</Error>}
           <Input
-            onChange={handleConfirmPasswordChange}
+            onChange={handleChange}
             type="password"
-            value={confirmPassword}
+            value={registerInfo.confirmPassword}
             name="confirmPassword"
             placeholder="confirm password"
           />
-          {errorMessage && <Error>{errorMessage}</Error>}
+          {errorMessage.confirmPassword && (
+            <Error>{errorMessage.confirmPassword}</Error>
+          )}
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
-          <Button>CREATE</Button>
+          <Button disabled={!readyForSubmit}>CREATE</Button>
         </Form>
       </Wrapper>
     </Container>
@@ -131,8 +212,7 @@ const Form = styled.form`
 `;
 
 const Input = styled.input`
-  flex: 1;
-  min-width: 40%;
+  width: 100%;
   padding: 10px;
   margin: 20px 10px 0px 0px;
 `;
@@ -149,6 +229,11 @@ const Button = styled.button`
   background-color: teal;
   color: white;
   cursor: pointer;
+
+  &:disabled {
+    background: gray;
+    cursor: default;
+  }
 `;
 
 const Error = styled.div`
